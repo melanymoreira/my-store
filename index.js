9a69aa013f1f112d2f9892a8b15af209402b5db3
Paulo Galarza <paulo-123@hotmail.es>
2024-12-09 08:54:16 -0500

    segundo commit:Sistema de ruteo

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("My Store");
+const express = require('express'); //Importar express
+const app = express(); // Asignar express a mi aplicación
+const port = 3000; // Asignación puerto donde se ejecutará el proy
+
+app.get('/', (req, res) => {
+  res.send('Hola servidor de express');
+});
+app.get('/nueva-ruta', (req, res) => {
+  res.send('Hola, soy una nueva ruta');
+});
+app.get('/products', (req, res) => {
+  res.json({
+    name: 'Producto 1',
+    price: 200,
+  });
+});
+
+app.listen(port, () => {
+  console.log('Mi puerto' + port);
+});
