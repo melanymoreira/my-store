a217d0288825ccbab1b3ccc2c1e1f19cb7022c90
Paulo Galarza <paulo-123@hotmail.es>
2024-12-18 10:51:31 -0500

    Tercer commit: Implementacion de datos falsos con faker-js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express'); //Importar express
+const { faker } = require('@faker-js/faker');
 const app = express(); // Asignar express a mi aplicación
 const port = 3000; // Asignación puerto donde se ejecutará el proy
 
@@ -9,9 +10,43 @@ app.get('/nueva-ruta', (req, res) => {
   res.send('Hola, soy una nueva ruta');
 });
 app.get('/products', (req, res) => {
+  const products = [];
+  for (let index = 0; index < 100; index++) {
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(), 10),
+      /* image: faker.image.imageUrl(), */
+    });
+  }
+  res.json(products);
+});
+
+app.get('/products/:id', (req, res) => {
+  const { id } = req.params;
   res.json({
-    name: 'Producto 1',
-    price: 200,
+    id: id,
+    name: 'Producto 2',
+    price: 120,
+  });
+});
+
+app.get('/users', (req, res) => {
+  const { limit, offset } = req.query;
+  if (limit && offset) {
+    res.json({
+      limit,
+      offset,
+    });
+  } else {
+    res.send('No hay parametros establecidos en el endpoint');
+  }
+});
+
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+  const { categoryId, productId } = req.params;
+  res.json({
+    categoryId,
+    productId,
   });
 });
 
