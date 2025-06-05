a7a0fa26ad5891f2a77965d89ce5bf2304ad0983
melanymoreira <rosmerymor11@gmail.com>
2025-06-04 21:51:02 -0500

    Conexion Exitosa-Tablas

--- config/config.js
+++ config/config.js
@@ -1,11 +1,11 @@
-require('dotenv').config();
+require('dotenv').config(); 
 const config = {
   env: process.env.NODE_ENV || 'dev',
   port: process.env.PORT || 3000,
   dbUser: process.env.DB_USER,
   dbPass: process.env.DB_PASS,
-  dbName: process.env.DB_NAME,
   dbHost: process.env.DB_HOST,
+  dbName: process.env.DB_NAME,
   dbPort: process.env.DB_PORT,
 };
 
--- db/config/config.js
+++ db/config/config.js
@@ -1,10 +1,9 @@
 require('dotenv').config();
-
 const config = {
   env: process.env.NODE_ENV || 'dev',
   port: process.env.PORT || 3000,
   dbUser: process.env.DB_USER,
-  dbPassword: process.env.DB_PASSWORD,
+  dbPass: process.env.DB_PASS,
   dbHost: process.env.DB_HOST,
   dbName: process.env.DB_NAME,
   dbPort: process.env.DB_PORT,
